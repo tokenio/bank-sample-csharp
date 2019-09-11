fe49bc74a69346d2a8ea66819cc1f284baa102b1
Vanika <vanika@appperfect.com>
2019-09-11 12:39:11 +0530

    added GenerateKeyTest

---
+++ test/GenerateKeyTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Tokenio.Sdk.Security;
+using Xunit;
+
+namespace Tokenio.BankSample
+{
+    public class GenerateKeyTest
+    {
+        [Fact]
+        public void Generate()
+        {
+            SecretKeyPair keyPair = SecretKeyPair.Create();
+            Console.WriteLine("crypto: EDDSA");
+            Console.WriteLine("private-key: "
+                    + keyPair.PrivateKeyString()
+                    + " // Used for signing bank auth payloads");
+            Console.WriteLine("public-key: "
+                    + keyPair.PublicKeyString()
+                    + "  // Give to Token so that Token can verify bank auth payloads");
+        }
+    }
+}
